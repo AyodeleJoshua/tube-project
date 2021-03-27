9ce323d685d904ac42feac2693b5ba1e31ca48dd
Joshua Ayodele <jayodele64@gmail.com>
2021-03-27 17:07:37 +0100

    added

--- src/apis/Youtube.js
+++ src/apis/Youtube.js
@@ -1,13 +1,12 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const KEY= "AIzaSyCaEeeVfyb8juD6oi-a5J3eQrc87ah_3cs"
+const KEY = 'AIzaSyCBx0bQ9LNv2mDqi2gCa4_2Qo1Wq2zHyVg';
 
 export default axios.create({
-    baseURL:"https://www.googleapis.com/youtube/v3",
-    params:{
-        part: 'snippet',
-        maxResults: 10,
-        key: KEY,
-        
-    }
-})
+	baseURL: 'https://www.googleapis.com/youtube/v3',
+	params: {
+		part: 'snippet',
+		maxResults: 10,
+		key: KEY,
+	},
+});
